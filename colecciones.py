2bdaffc139fc753243f8b1f510975a53ca2851bc
CamiloWiesner <gironwiesnerjuancamilo11@outlook.com>
2025-10-12 23:43:00 +0200

    empezando a aprender sobre las colecciones, esto son los diccionarios

---
+++ colecciones.py
@@ -0,0 +1,14 @@
+diccionario = {"clave1": "Valor", "clave2": "Otro Valor"}
+print(diccionario)
+diccionarios = {"animal": "gato", "cosa": "piedra", "planta": "lechuga"}
+
+print(diccionarios)
+
+print(diccionarios["animal"]) # Mostrará "gato"
+
+print(diccionarios["planta"]) # Mostrará "lechuga"
+
+diccionarios["planta"] = "coliflor"
+
+print(diccionarios["planta"]) # Ahora mostrará "coliflor"
+
