1ee2e40c19d165830c41f83e3f4298b84f6f4511
CamiloWiesner <gironwiesnerjuancamilo11@outlook.com>
2025-10-22 21:09:39 +0200

    Doy por terminado el tema de diccionarios por ahora

--- colecciones.py
+++ colecciones.py
@@ -47,6 +47,7 @@ print(dicci)
 coordenadas = {(0,0): "Eje", (0,1):"universidad", (1,1):"raiz"} # las claves de un diccionario deben ser objetos hashables. 
 print(coordenadas[(0,1)])
 
+
 referencias = {
     "libros": ({"titulo": "Juego de Pythons", "autor": "G. R. R. Python"},
                {"titulo": "Un Mago de Python", "autor": "U. K. Python"},
@@ -61,5 +62,58 @@ tercera_pelicula = referencias["peliculas"][2]
 
 print('El segundo libro se titula "{}"'.format(segundo_libro))
 print('La tercera película se titula "{}"'.format(tercera_pelicula))
+
+numeritos = {
+    "uno": 1,
+    "dos": 2,
+    "tres": 3,
+    "cuatro": 4,
+    "cinco": 5
+}
+
+print(numeritos)
+del numeritos["tres"] # Con del podemos borrar valores de los diccionarios
+print(numeritos)
+
+direcciones = {"N": "Norte", "S":"Sur", "E":"Este", "O":"Oeste"}
+print(len(direcciones)) # Con len() podemos retornar el número de elementos que contiene un diccionario
+
+diccionario_a = {
+    "animal": "gato",
+    "cosa": "piedra",
+    "planta": "lechuga"
+}
+
+diccionario_b = {
+    "mineral": "cuarzo",
+"cosa": "pelusa",
+    "metal": "cobre"
+}
+
+union = diccionario_a | diccionario_b # Con (|) podemos hacer la unión de dos diccionarios
+print(union) 
+
+diccionario_c = {
+    "animal": "gato",
+    "cosa": "piedra",
+    "planta": "lechuga"
+}
+
+diccionario_d = {
+    "mineral": "cuarzo",
+    "cosa": "pelusa",
+    "metal": "cobre"
+}
+
+diccionario_c |= diccionario_d # Con != en vez de unificar modificamos el primero de ellos 
+print(diccionario_c)
+
+usuario = {"nombre": "Pablo", "username": "psicobyte"}
+correo = usuario.get("correo","email@email.email") # Con el metodo get() retornará el valor asociado si es que esa clave existe, si no devolverá un keyError (se le puede pasar un segundo argumento)
+print(correo)
+
+usuarios = {"nombre": "Pablo", "username": "psicobyte"}
+print(usuarios.setdefault("correo", "email@email.email"))
+print(usuarios)
 
  
